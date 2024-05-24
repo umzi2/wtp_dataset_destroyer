e0507e3ae051957785259613f50f874e84ca0af1
umzi <ab5447612@gmail.com>
2024-05-24 12:42:08 +0300

    fix: halo safe_random and probability

--- instructions/halo.md
+++ instructions/halo.md
@@ -2,20 +2,20 @@
 {
       "type": "halo",
       "type_halo": ["laplacian","unsharp_mask"],
-      "kernel": [0,3,1],
+      "kernel": [0,3],
       "sharpening_factor": [0, 2],
       "amount": [0,1],
       "threshold": [0,20]
 }    
 ```
 - type_halo - halo overlay algorithm list
-- - kernel - applies median blur with a random kernel [low,high] necessary to reduce artifacts
+- kernel - applies median blur with a random kernel `list[float|int]` necessary to reduce artifacts
 - laplacian:
   - sharpening_factor - sharpness from which we get halo
   - laplacian - sheet from which the random kernel size for the Laplace filter is taken
 - unsharp_mask:
   - amount - halo force 
   - threshold - halo effect threshold
-- probably* - chance of triggering
+- probability* - chance of triggering
 
 ps This piece from the image to which sharpening was applied takes only the white halo and it comes out at 1 bit, so it’s better to use it before blurring
--- src/process/halo_degr.py
+++ src/process/halo_degr.py
@@ -2,7 +2,8 @@ import numpy as np
 from numpy import random
 import cv2 as cv
 from .utils import probability
-
+from .custom_blur import box_blur
+from ..utils.random import safe_uniform
 from ..utils.registry import register_class
 
 
@@ -19,7 +20,7 @@ class Halo:
                     Defaults to [0, 2].
                 - "laplacian" (list of int, optional): List of Laplacian kernel sizes.
                     Defaults to [3].
-                - "probably" (float, optional): Probability of applying halo loss reduction. Defaults to 1.0.
+                - "probability" (float, optional): Probability of applying halo loss reduction. Defaults to 1.0.
                 - "type_halo" (list of str, optional): List of halo loss reduction types.
                     Defaults to ["laplacian"].
                 - "amount" (list of float, optional): Range of amounts for unsharp mask.
@@ -32,24 +33,22 @@ class Halo:
         self.factor = halo_loss_dict.get("sharpening_factor", [0, 2])
         self.kernel = halo_loss_dict.get("kernel", [0, 2])
         self.laplacian = halo_loss_dict.get("laplacian", [3])
-        self.probably = halo_loss_dict.get("probably", 1.0)
+        self.probability = halo_loss_dict.get("probability", 1.0)
         self.type = halo_loss_dict.get("type_halo", ["laplacian"])
         self.amount = halo_loss_dict.get("amount", [1, 1])
         threshold = halo_loss_dict.get("threshold", [0, 0])
         self.threshold = [threshold[0] / 255, threshold[1] / 255]
 
     def __laplacian(self, lq: np.ndarray) -> np.ndarray:
-        lq = np.squeeze(lq).astype(np.float32)
         if np.ndim(lq) != 2:
             img_gray = cv.cvtColor(lq, cv.COLOR_RGB2GRAY)
         else:
             img_gray = lq
-        sharpening_factor = random.randint(*self.factor)
-        kernel = random.randint(*self.kernel)[0]
+        sharpening_factor = safe_uniform(self.factor)
+        kernel = safe_uniform(self.kernel)
         laplacian = random.choice(self.laplacian)
-        img_gray = img_gray
         if kernel:
-            img_gray = cv.blur(img_gray, ksize=[kernel, kernel])
+            img_gray = box_blur(img_gray, kernel)
         laplacian = cv.Laplacian(img_gray, cv.CV_32F, ksize=laplacian)
         sharpened_image = img_gray - sharpening_factor * laplacian
         _, sharpened_image = cv.threshold(sharpened_image, 0.98, 1, 0, cv.THRESH_BINARY)
@@ -58,13 +57,10 @@ class Halo:
         return np.clip(lq + sharpened_image, 0, 1).astype(np.float32)
 
     def __unsharp_mask(self, lq: np.ndarray) -> np.ndarray:
-        kernel_size = np.random.randint(*self.kernel)[0]
-        amount = np.random.uniform(*self.amount)
-        threshold = np.random.uniform(*self.threshold)
-        if kernel_size % 2 == 0:
-            kernel_size += 1
-
-        blurred = cv.GaussianBlur(lq, (kernel_size, kernel_size), 0)
+        sigma =safe_uniform(self.kernel)
+        amount = safe_uniform(self.amount)
+        threshold = safe_uniform(self.threshold)
+        blurred = cv.GaussianBlur(lq, (0, 0), sigmaX=sigma, sigmaY=sigma, borderType=cv.BORDER_REFLECT)
         sharpened = np.clip(float(amount + 1) * lq - float(amount) * blurred, 0, 1)
         if threshold > 0:
             low_contrast_mask = np.absolute(lq - blurred) < threshold
@@ -82,7 +78,7 @@ class Halo:
             tuple: A tuple containing the halo loss reduced low-quality image and the corresponding high-quality image.
         """
         try:
-            if probability(self.probably):
+            if probability(self.probability):
                 return lq, hq
             type_halo = np.random.choice(self.type)
             if type_halo == "laplacian":
