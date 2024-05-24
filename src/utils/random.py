5de03ab2ebfef93cc49f91d021d985cd1cfe437f
umzi <ab5447612@gmail.com>
2024-05-24 06:29:01 +0300

    add: safe_random

---
+++ src/utils/random.py
@@ -0,0 +1,26 @@
+from numpy.random import uniform, randint
+import numpy as np
+
+
+def safe_uniform(rand_list: list[float]) -> float:
+    if len(rand_list) == 1:
+        return rand_list[0]
+    if rand_list[0] >= rand_list[1]:
+        return rand_list[0]
+    return uniform(*rand_list)
+
+
+def safe_randint(rand_list: list[int]) -> int:
+    if len(rand_list) == 1:
+        return rand_list[0]
+    if rand_list[0] >= rand_list[1]:
+        return rand_list[0]
+    return randint(*rand_list)
+
+
+def safe_arange(range_list: list) -> np.ndarray:
+    if len(range_list) == 1:
+        return range_list[0]
+    if range_list[0] >= range_list[1]:
+        return range_list[0]
+    return np.arange(*range_list)
