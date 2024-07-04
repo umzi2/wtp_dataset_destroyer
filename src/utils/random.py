058de5b3a6a74c1aa183fdc8732ba7b1c4262f23
umzi <ab5447612@gmail.com>
2024-07-04 21:00:23 +0300

    ruff fix

--- src/utils/random.py
+++ src/utils/random.py
@@ -3,28 +3,22 @@ import numpy as np
 
 
 def safe_uniform(rand_list: list[float] | float) -> float:
-    if list != type(rand_list):
+    if not isinstance(rand_list, list):
         return rand_list
-    if len(rand_list) == 1:
-        return rand_list[0]
-    if rand_list[0] >= rand_list[1]:
+    if len(rand_list) == 1 or rand_list[0] >= rand_list[1]:
         return rand_list[0]
     return uniform(rand_list[0], rand_list[1])
 
 
 def safe_randint(rand_list: list[int] | int) -> int:
-    if list != type(rand_list):
+    if not isinstance(rand_list, list):
         return rand_list
-    if len(rand_list) == 1:
-        return rand_list[0]
-    if rand_list[0] >= rand_list[1]:
+    if len(rand_list) == 1 or rand_list[0] >= rand_list[1]:
         return rand_list[0]
     return randint(rand_list[0], rand_list[1])
 
 
 def safe_arange(range_list: list) -> np.ndarray:
-    if len(range_list) == 1:
-        return [range_list[0]]
-    if range_list[0] >= range_list[1]:
-        return [range_list[0]]
+    if len(range_list) == 1 or range_list[0] >= range_list[1]:
+        return np.ndarray([range_list[0]])
     return np.arange(*range_list)
